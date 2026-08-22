bc4bdb4bbb671829e487bd043bf18e0d278d689c
Abhi <abhishekr12j@gmail.com>
2026-08-22 13:53:00 +0530

    Time: 13 ms (13.62%) | Memory: 43.9 MB (73.45%) - LeetSync

---
+++ 401-binary-watch/binary-watch.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> times = new ArrayList<>();
+        
+        for (int h = 0; h < 12; h++) {
+            for (int m = 0; m < 60; m++) {
+                // Total LEDs turned on = active bits in hours + active bits in minutes
+                if (Integer.bitCount(h) + Integer.bitCount(m) == turnedOn) {
+                    times.add(String.format("%d:%02d", h, m));
+                }
+            }
+        }
+        
+        return times;
+    }
+}
