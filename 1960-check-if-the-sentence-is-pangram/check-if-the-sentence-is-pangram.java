1ef5cd4e3bf4f8f2f5b1508caa41810021272f47
Abhi <abhishekr12j@gmail.com>
2026-08-24 02:37:39 +0530

    Time: 2 ms (63.34%) | Memory: 42.8 MB (69.26%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        int [] count=new int[26];
+        for(int i=0;i<sentence.length();i++){
+            int index=sentence.charAt(i)-'a';
+            count[index]++;
+        }
+        for(int i=0;i<26;i++){
+            if(count[i]==0) return false;
+        }
+        return true;
+    }
+}
