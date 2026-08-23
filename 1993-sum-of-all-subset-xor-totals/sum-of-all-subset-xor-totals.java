34a4a4386d69e25a29017bf16b4caf2df24aa864
Abhi <abhishekr12j@gmail.com>
2026-08-23 14:46:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (76.82%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,19 +1,9 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        int n =1<<nums.length;
-        int ans=0;
-        
-        for(int i=0;i<n;i++){
-            int xor=0;
-            for(int j=0;j<nums.length;j++){
-                if((i&(1<<j))!=0)
-                 xor ^=nums[j];
-            }
-          ans+=xor;
-
-           
-
+        int totalOr = 0;
+        for (int num : nums) {
+            totalOr |= num;
         }
-        return ans;
+        return totalOr * (1 << (nums.length - 1));
     }
 }
