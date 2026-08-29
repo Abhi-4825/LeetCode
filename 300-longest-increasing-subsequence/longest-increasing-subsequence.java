ed3a81ab35b2e5ab6becc35e4e854420be838c77
Abhi <abhishekr12j@gmail.com>
2026-08-29 22:59:51 +0530

    Time: 37 ms (74.15%) | Memory: 46 MB (70.91%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,27 +1,22 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-       List<Integer> l=new ArrayList<>();
-       l.add(nums[0]);
-       for(int i=1;i<nums.length;i++){
-            if(nums[i] > l.get(l.size()-1))
-             l.add(nums[i]);
-            else{
-               int index=findCeiling(l,nums[i]);
-               l.set(index,nums[i]);
-            } 
-           
-       }
-        return l.size();
-    }
-    private int findCeiling(List<Integer> list,int k){
-            int start=0;
-            int end=list.size()-1;
-            while(start<=end){
-                int mid=start+(end-start)/2;
-                if(k==list.get(mid)) return mid;
-                else if(k < list.get(mid)) end=mid-1;
-                else start=mid+1;
+        int n=nums.length;
+        int[] dp =new int[n];
+        Arrays.fill(dp,1);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i])
+                 dp[i]=Math.max(dp[i],dp[j]+1);
             }
-            return start;
+        }
+        int answer=0;
+        for(int num:dp){
+            answer=Math.max(answer,num);
+        }
+        return answer;
+
+
+
+
     }
 }
