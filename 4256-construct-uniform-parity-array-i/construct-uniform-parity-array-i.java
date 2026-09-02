6492a897685d1242ba0f93ab36c9ba7a00c87ccd
Abhi <abhishekr12j@gmail.com>
2026-09-02 23:21:52 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (57.87%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,34 +1,5 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        // we are going to check if all odd or odd even case possible 
-
-        boolean[] odd=new boolean[nums1.length];
-        boolean[] even =new boolean[nums1.length];
-
-        for(int i=0;i<nums1.length;i++){
-            if((nums1[i]&1)==1) odd[i]=true;
-            else{
-                for(int j=0;j<nums1.length;j++){
-                    if(j!=i && ((nums1[i]-nums1[j])&1)==1)odd[i]=true;
-                }
-            }
-            if((nums1[i]&1)==0) even[i]=true;
-            else{
-                for(int j=0;j<nums1.length;j++){
-                 if(j!=i && ((nums1[i]-nums1[j])&1)==0)even[i]=true;
-                }
-            }
-        }
-        boolean allOdd=true;
-        boolean allEven=true;
-        for(int i=0;i<nums1.length;i++){
-            if(!odd[i])allOdd=false;
-            if(!even[i]) allEven=false;
-        }
-        return allOdd || allEven;
-
-
-
+        return true;
     }
-
 }
