a1124076cb920d75c115d588b3e11e9351e11d47
Abhi <abhishekr12j@gmail.com>
2026-09-23 19:45:08 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (6.01%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,8 +1,15 @@
 class Solution {
     public int rob(int[] nums) {
         int [] dp=new int[nums.length];
-        Arrays.fill(dp,-1);
-       return rob(nums,nums.length-1,dp);
+        dp[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            int notPick=dp[i-1];
+            int pick=nums[i];
+            if(i>1)
+             pick=nums[i]+dp[i-2];
+            dp[i]=Math.max(pick,notPick); 
+        }
+        return dp[nums.length-1];
     }
     // memoization 
     private int rob(int[] nums,int index,int[] dp){
