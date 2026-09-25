c8cd0514263951dc31414bd61e64649b4f7b7708
Abhi <abhishekr12j@gmail.com>
2026-09-25 20:33:24 +0530

    Time: 1 ms (8.09%) | Memory: 43.8 MB (15.94%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -1,47 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-
-        Queue<TreeNode> q = new LinkedList<>();
+        if(root==null) return true;
+        Queue<TreeNode> q=new LinkedList<>();
         q.offer(root);
-
-        while (!q.isEmpty()) {
-
-            int size = q.size();
-            TreeNode[] arr = new TreeNode[size];
-
-            for (int i = 0; i < size; i++) {
-                TreeNode curr = q.poll();
-
-                arr[i] = curr;
-
-                if (curr != null) {
+        while(!q.isEmpty()){
+            int s=q.size();
+            TreeNode[] arr=new TreeNode[s];
+            for(int i=0;i<s;i++){
+                TreeNode curr=q.poll();
+                 arr[i]=curr;
+                if(curr!=null){
+                   
                     q.offer(curr.left);
                     q.offer(curr.right);
                 }
             }
-
-            int left = 0;
-            int right = size - 1;
-
-            while (left < right) {
-
-                if (arr[left] == null && arr[right] == null) {
-                    left++;
-                    right--;
-                    continue;
+            
+            int i=0;
+            int j=arr.length-1;
+            while(i<j){
+                if(arr[i]!=null && arr[j]!=null){
+                   if(arr[i].val!=arr[j].val) return false;
                 }
-
-                if (arr[left] == null || arr[right] == null)
-                    return false;
-
-                if (arr[left].val != arr[right].val)
-                    return false;
-
-                left++;
-                right--;
+                if((arr[i]==null && arr[j]!=null) || (arr[i]!=null && arr[j]==null) ) return false;
+                i++;
+                j--;
+               
+                
             }
-        }
-
-        return true;
+       }
+       return true;
     }
 }
